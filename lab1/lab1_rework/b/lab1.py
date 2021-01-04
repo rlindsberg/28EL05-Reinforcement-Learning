5ecbff6a4285ffdf30ff17f163f2c71cefa33155
rlindsberg <roderick@karlemstrand.com>
2021-01-04 21:08:18 +0100

    Add code for running 100 games with 1-20 time horizon

--- lab1/lab1_rework/b/lab1.py
+++ lab1/lab1_rework/b/lab1.py
@@ -1,6 +1,7 @@
 import numpy as np
 import lab1_maze as mz
 from matplotlib import pyplot as plt
+import csv
 
 
 def define_maze():
@@ -44,10 +45,46 @@ def run_game(maze, env, horizon):
     return path
 
 
+def get_game_result(path):
+    for position in path:
+        # x is the horizontal axis and y is vertical axis
+        # top-left is 0,0
+        player_x = position[0]
+        player_y = position[1]
+        minotaur_x = position[2]
+        minotaur_y = position[3]
+
+        if player_x == minotaur_x and player_y == minotaur_y:
+            return "lose"
+
+        if player_x == 6 and player_y == 5:
+            return "win"
+
+    return "time"
+
+
 def main():
-    maze, env = init_game()
-    path = run_game(maze, env, horizon=20)
-    animate_game_replay(maze, path)
+    statistics_array = []
+
+    # time horizon is 1-20
+    for time_horizon in range(1, 21):
+        stats = {"time_horizon": time_horizon, "win": 0, "lose": 0, "time": 0}
+
+        # run 100 games
+        for game in range(100):
+            maze, env = init_game()
+            path = run_game(maze, env, horizon=time_horizon)
+            result = get_game_result(path)
+            stats[result] += 1
+
+        # save results of 100 games to array
+        statistics_array.append(stats)
+
+    with open('lab1_b.csv', mode='w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for stat in statistics_array:
+            win_rate = stat["win"] / 100
+            csv_writer.writerow([stat["time_horizon"], win_rate])
 
 
 if __name__ == '__main__':
